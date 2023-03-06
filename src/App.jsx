e1b9bbd21064535ed93d11dbebd0c94282edcdcb
Jitendra Asole <40131393+jay112298@users.noreply.github.com>
2023-03-06 18:29:46 +0530

    Add Create and View pages with routes

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,10 @@ import './App.css'
 import Header from './components/Header/Header'
 import Title from './components/Title/Title'
 import Navigation from './components/Navigation/Navigation'
+import Flashcards from './pages/Flashcards/Flashcards'
+import Create from './pages/Create/Create'
+import { Route, Routes } from 'react-router-dom'
+
 
 function App() {
   const [count, setCount] = useState(0)
@@ -14,6 +18,10 @@ function App() {
       <div className="container">
         <Title />
         <Navigation />
+        <Routes>
+          <Route path='/' element={<Create />}/>
+          <Route path='/my-flashcards' element={<Flashcards />}/>
+        </Routes>
       </div>
       
     </div>
--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+// import Routes from '../../Routes'
 import { Navtab } from './Navigation.st'
+
 
 const Navigation = () => {
   return (
     <Navtab>
-        <div className="create">Create New</div>
-        <div className="display">My Flashcards</div>
+        <div className="create"><Link to="/">Create New</Link></div>
+        <div className="display"><Link to="/my-flashcards">My Flashcards</Link></div>
     </Navtab>
   )
 }
